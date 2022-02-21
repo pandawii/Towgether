75b44e0a03244224bec1a60d1ddf211f89ff4117
Abdelhakim mohamed <kemstroarab@gmail.com>
2022-02-21 19:07:32 +0200

    Update Trap.cs

--- Towgether/Assets/Scripts/Trap.cs
+++ Towgether/Assets/Scripts/Trap.cs
@@ -8,7 +8,7 @@ public class Trap : MonoBehaviour
     {
         if (col.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Dead");
+            Debug.Log("Deadl");
         }
     }
 }
