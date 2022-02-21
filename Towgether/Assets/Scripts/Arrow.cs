0c89ca2dacfebdca2d5629799c8e04512040b7a7
Abdelhakim mohamed <kemstroarab@gmail.com>
2022-02-21 21:03:46 +0200

    value tweaks

--- Towgether/Assets/Scripts/Arrow.cs
+++ Towgether/Assets/Scripts/Arrow.cs
@@ -9,11 +9,10 @@ public class Arrow : MonoBehaviour
      void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Player"))
-        {
-            
-            gameObject.GetComponent<SpriteRenderer>().enabled = false;
-            Instantiate(GameAssets.Getinstance().Explosion, gameObject.transform.position, Quaternion.identity);
+        {          
             Destroy(gameObject);
+            Instantiate(GameAssets.Getinstance().Explosion, gameObject.transform.position, Quaternion.identity);
+            
         }
     }
 
