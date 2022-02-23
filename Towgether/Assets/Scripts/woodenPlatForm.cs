50a49b356282560e523a0e49e95f1c0d0b603d15
Abdelhakim mohamed <kemstroarab@gmail.com>
2022-02-23 16:00:36 +0200

    values

--- Towgether/Assets/Scripts/woodenPlatForm.cs
+++ Towgether/Assets/Scripts/woodenPlatForm.cs
@@ -6,29 +6,20 @@ public class woodenPlatForm : MonoBehaviour
 {
 
     // Use this for initialization
-     float speed = 4f;
-     float RotAngleYMin = 45;
-     float RotAngleYMax = -45;
-    bool PlayerTouched;
+     
+
     void Start()
     {
-        PlayerTouched = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (PlayerTouched) { 
-            float rY = Mathf.SmoothStep(RotAngleYMin, RotAngleYMax, Mathf.PingPong(Time.time * speed, 1));
-        transform.rotation = Quaternion.Euler(0, 0, rY);
+         
+            
                 
-        }
+        
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            PlayerTouched = true;
-        }
-    }
+    
 }
