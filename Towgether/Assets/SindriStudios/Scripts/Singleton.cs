cdde9d04a8eeaa8dfb39ea9c53038642bfb3b6ff
Abdelhakim mohamed <kemstroarab@gmail.com>
2022-03-15 15:22:06 +0200

    values

---
+++ Towgether/Assets/SindriStudios/Scripts/Singleton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+
+    public static Singleton Instance
+    {
+        get;
+        private set;
+    }
+    public virtual void Awake()
+    {
+        Instance = this;
+    }
+}
